27ee7ea80bdbf1ea0f3e93c7a785aceeeec0a2cc
Michal Jaworski <borzoj@gmail.com>
2020-09-11 11:42:31 +0100

    iterate

--- balance-brackets/balance-brackets.js
+++ balance-brackets/balance-brackets.js
@@ -1,4 +1,10 @@
 function balanceBrackets(string) {
+    const chars = string.split("");
+    console.log(chars);
+    for (var i = 0; i < chars.length; i++) {
+        const currentChar = chars[i];
+        //Do something
+    }
     return true;
 }
 
