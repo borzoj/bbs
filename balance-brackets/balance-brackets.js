6e730a012104f8cbce45d999d0181dee17133b2c
Michal Jaworski <borzoj@gmail.com>
2020-09-11 12:14:42 +0100

    boom

--- balance-brackets/balance-brackets.js
+++ balance-brackets/balance-brackets.js
@@ -1,11 +1,31 @@
-function balanceBrackets(string) {
-    const chars = string.split("");
-    console.log(chars);
-    for (var i = 0; i < chars.length; i++) {
-        const currentChar = chars[i];
-        //Do something
+function bracketMatches(opening, closing) {
+    if (opening == '(' && closing == ')') {
+        return true
     }
-    return true;
+    if (opening == '{' && closing == '}') {
+        return true
+    }
+    if (opening == '[' && closing == ']') {
+        return true
+    }
+    return false
+}
+
+function balanceBrackets(string) {
+    let stack = []
+    const chars = string.split("")
+    for (var i = 0; i < chars.length; i++) {
+        const currentChar = chars[i]
+        if (currentChar=='{' || currentChar=='[' || currentChar=='(') {
+            stack.push(currentChar)
+        } else if (currentChar=='}' || currentChar==']' || currentChar==')') {
+            const topChar = stack.pop();
+            if (!bracketMatches(topChar, currentChar)) {
+                return false;
+            }
+        } 
+    }
+    return stack.length == 0;
 }
 
 module.exports = balanceBrackets;
--- balance-brackets/balance-brackets.test.js
+++ balance-brackets/balance-brackets.test.js
@@ -3,3 +3,36 @@ const balanceBrackets = require('./balance-brackets');
 test('[] is balanced', () => {
   expect(balanceBrackets('[]')).toBeTruthy();
 });
+
+test('[]{}() is balanced', () => {
+  expect(balanceBrackets('[]{}()')).toBeTruthy();
+});
+
+test('[({})]{}() is balanced', () => {
+  expect(balanceBrackets('[({})]{}()')).toBeTruthy();
+});
+
+test('({(){}[]})[]. is balanced', () => {
+  expect(balanceBrackets('({(){}[]})[].')).toBeTruthy();
+});
+
+test('{{[[(())]]}} is balanced', () => {
+  expect(balanceBrackets('{{[[(())]]}}')).toBeTruthy();
+});
+
+test('[(}] is not balanced', () => {
+  expect(balanceBrackets('[(}]')).toBeFalsy();
+});
+
+test('{[(])} is not balanced', () => {
+  expect(balanceBrackets('{[(])}')).toBeFalsy();
+});
+
+test('[[] is not balanced', () => {
+  expect(balanceBrackets('[[]')).toBeFalsy();
+});
+
+
+test('[]] is not balanced', () => {
+  expect(balanceBrackets('[]]')).toBeFalsy();
+});
